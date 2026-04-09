74263c2b45881175ae0c2bf93fb473c0cd02e0f7
Kashi7 <baotoan08020802@gmail.com>
2026-04-09 19:30:56 +0700

    Thêm chức năng xoá đăng tạo playlists,songs,albums

--- Emuzik_DB.sql
+++ Emuzik_DB.sql
@@ -65,4 +65,20 @@ create table Follow (
     foreign key (Follower_id) references Users(User_id),
     foreign key (Followed_id) references Artists(Artist_id)
 );
-    
+
+
+-- ==========================================
+-- THÊM DỮ LIỆU MẪU TỪ FILE HTML (Từng Ngày Yêu Em)
+-- ==========================================
+
+-- 1. Thêm nghệ sĩ (buitruonglinh)
+INSERT INTO Artists (Artist_id, Artist_name, Biography, Avatar_url) 
+VALUES (1, 'buitruonglinh', 'Nghệ sĩ Indie Việt Nam', './img/artist/artist_buitruonglinh.jpg');
+
+-- 2. Thêm Album/Single (để liên kết với bài hát vì bảng Songs có tham chiếu tới Album_id)
+INSERT INTO Albums (Album_id, Album_title, Release_date, Cover_image_url, Artist_id) 
+VALUES (1, 'Từng Ngày Yêu Em (Single)', '2024-01-01', './img/song/song_buitruonglinh/song_tungngayyeuem/poster.jpg', 1);
+
+-- 3. Thêm bài hát (Từng Ngày Yêu Em) kèm toàn bộ lời bài hát (Lyrics)
+INSERT INTO Songs (Song_id, Song_title, File_url, Lyric, View_count, Album_id) 
+VALUES (1, 'Từng Ngày Yêu Em', 'https://www.nhaccuatui.com/song/0uvhkrobLlp0', 'Lại chìm trong đôi mắt em xoe tròn ngất ngây\nPhút giây khi mà anh khẽ nhìn sang\nLại làm đôi môi nhớ em lại muốn hôn em thêm bao lần\nTừng ngày cô đơn xé đôi, hạ thu đông khẽ trôi\nCạnh bên em anh sẽ thôi u sầu\nLại làm cho anh càng thấy yêu em hơn ngày qua\nChẳng phải gió cuốn mưa bay đưa đôi tay \nĐón anh về ngày lời yêu cất lên\nChỉ cần thức giấc khi bên em nơi anh thấy yên bình\nBiển rộng sông sâu anh trót thương chỉ riêng mình em thôi đấy\nTình yêu ấy chẳng thể đổi thay\nDù là bao lâu cảm xúc trong anh mãi đong đầy\nVội lạc vào giấc mơ nhẹ nhàng tựa ý thơ\nNụ cười người để anh nhớ để anh mong đợi\nSợ một ngày ngát xanh cuộc đời này vắng em\nAnh phải làm sao bây giờ ?\nEm này anh biết chăng bao ngày bao tháng năm\nChỉ cần một lần say đắm sẽ in hằn sâu trong tim anh\nMãi như vậy như lời hứa yêu em yêu mình em một đời\nTừ khi yêu em thế gian vương đầy sắc hoa\nTới nơi chân trời ta vẫn hằng mơ\nĐừng để cho tia nắng mai đặt chiếc hôn lên đôi vai gầy\nMặc kệ cơn dông bước ra điều gì phía trước ta\nTừng ngày bên nhau lướt qua êm đềm\nLại làm cho anh càng muốn yêu em không rời xa\nTình yêu đến ngọt ngào yêu áng mây trên cao\nTình yêu khẽ thì thầm anh thích em ra sao\nTình yêu muốn nồng nàn như sóng xô dạt dào khắp muôn nơi\nTừng ngày cô đơn xé đôi hạ thu đông khẽ trôi\nCạnh bên em anh sẽ thôi u sầu\nLại làm cho anh càng thấy yêu em\nLại làm mình ngất ngây nghẹn ngào từng phút giây\nCả bầu trời ngày hôm ấy bỗng như thu lại\nNhẹ nhàng rồi dắt tay\nAnh ngỏ lời muốn em sẽ là của anh sau này\nEm à anh biết chăng\nDẫu là bao tháng năm\nChỉ cần một lần say đắm sẽ in hằn sâu trong tim anh\nMãi như vậy như lời hứa yêu em\nyêu mình em một đời\nTình yêu đến ngọt ngào yêu áng mây trên cao\nTình yêu khẽ thì thầm anh thích em ra sao\nTình yêu muốn nồng nàn như sóng xô dạt dào khắp muôn nơi\nTừng ngày cô đơn xé đôi hạ thu đông khẽ trôi\nCạnh bên em anh sẽ thôi u sầu\nLại làm cho anh càng thấy yêu em\nLại làm mình ngất ngây nghẹn ngào từng phút giây\nCả bầu trời ngày hôm ấy bỗng như thu lại\nNhẹ nhàng rồi dắt tay\nAnh ngỏ lời muốn em sẽ là của anh sau này\nEm à anh biết chăng\nDẫu là bao tháng năm\nChỉ cần một lần say đắm sẽ in hằn sâu trong tim anh\nMãi như vậy như lời hứa yêu em yêu mình em một đời\nLà ri la, là ri la, la ri la ta ta\nLà ri la, là ri la, la ri la ta ta\nLời yêu thương mà lâu nay\nTừ con tim anh dành cho em\nSẽ không bao giờ đổi thay\nLà ri la, là ri la, la ri la ta ta\nLà ri la, là ri la, la ri la ta ta\nVì anh sẽ mãi như vậy\nNhư lời hứa yêu em\nYêu mình em một đời', 0, 1);
